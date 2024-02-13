a3d6d02345939b5f2894564698ad1421fedf6d88
TiagoJBO <timarao2@hotmail.com>
2024-02-13 20:01:12 -0300

    Project-react-burger

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import{Container,H1} from "./styles"
+import { Container, H1, Input, Button, Image, InputLabel } from "./styles";
+import LogoBurger from "./Assets/LogoBurger.png";
 
 const App = () => {
+  return (
+    <Container>
+      <Image alt="logo-burger" src={LogoBurger} />
+      <H1>Faça seu Pedido</H1>
+      <InputLabel>Pedido</InputLabel>
+      <Input placeholder="Pedido" />
 
-
-  return <Container>
-    <H1>Olá Projeto Hamburgueria </H1>
-    </Container>;
-  
+      <InputLabel>Nome do Cliente</InputLabel>
+      <Input placeholder="Nome do Cliente" />
+      <Button>Novo Pedido</Button>
+    </Container>
+  );
 };
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import GlobalStyled from "./globalStyled";
 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App /> );
+root.render(
+  <>
+    <App />
+    <GlobalStyled />
+  </>
+);
